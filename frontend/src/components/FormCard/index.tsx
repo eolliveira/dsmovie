f7c737e218b47a554198c6b440cc5fe33ac1a47d
Erick Oliveira <erickoliveira@rorato.local>
2022-01-18 16:08:19 -0300

    axios /PUT

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -23,19 +23,35 @@ const FormCard = ( { movieId} : Props ) => {
             });
     }, [movieId]);
 
+    const scoreUpdate = {
+        movieId: movie?.id,
+        email: '',
+        score: ''
+    }
+
+    const SaveScore = () => {
+        axios.put(`${BASE_URL}/scores`, scoreUpdate)
+        .then((response) => {
+            console.log(`Filme atualizado com sucesso: ${response.data}`);
+        })
+        .catch((error) => {
+            console.log(`Não foi possivel obter obter dados do filme: ${error}`);
+        });
+    }
+
     return (
         <div className='dsmovie-container-form'>
             <div className="card dsmovie-form-card mt-4 mb-5">
                 <img className="card-img-top rounded-top" src={movie?.image} alt="" />
                 <p className="dsmovie-card-text mt-3">{movie?.title}</p>
-                <form className='container mt-2'>
+                <form onSubmit={(event) => { event.preventDefault(); SaveScore() }} className='container mt-2'>
                     <div className="form-group">
                         <label className='dsmovie-email' htmlFor="dsmovie-form-email">Informe seu email</label>
-                        <input type="email" className="form-control dsmovie-input-email" id="dsmovie-form-email" aria-describedby="emailHelp"></input>
+                        <input type="email" className="form-control dsmovie-input-email" id="dsmovie-form-email" aria-describedby="emailHelp" onChange={(event) =>{ scoreUpdate.email = event.target.value }}></input>
                     </div>
                     <div className="form-group mt-3">
                         <label className='dsmovie-text-score' htmlFor="dsmovie-form-score">Informe sua avaliação</label>
-                        <select className="form-control dsmovie-select-score" id='dsmovie-form-score'>
+                        <select className="form-control dsmovie-select-score" id='dsmovie-form-score' onChange={(event) => { scoreUpdate.score = event.target.value }}>
                             <option>1</option>
                             <option>2</option>
                             <option>3</option>
@@ -45,7 +61,7 @@ const FormCard = ( { movieId} : Props ) => {
                     </div>
                     <div className='dsmovie-buttons'>
                         <button type="submit" className='dsmovie-btn-salvar'>Salvar</button>
-                        <Link to='/' className='dsmovie-btn-cancelar'>Cancelar</Link>
+                        <button type='submit' className='dsmovie-btn-cancelar'>Cancelar</button>
                     </div>
                 </form>
             </div>
