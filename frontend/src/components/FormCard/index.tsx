3584793d1d682373491830a28127cb6ccc91ebf3
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-01-19 22:12:07 -0300

    form corrections

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/Requests';
 import './styles.css';
@@ -9,7 +9,7 @@ type Props = {
     movieId: string;
 }
 
-const FormCard = ( { movieId} : Props ) => {
+const FormCard = ({ movieId }: Props) => {
 
     const [movie, setMovie] = useState<Movie>();
 
@@ -23,20 +23,20 @@ const FormCard = ( { movieId} : Props ) => {
             });
     }, [movieId]);
 
-    const scoreUpdate = {
-        movieId: movie?.id,
-        email: '',
-        score: ''
-    }
+    const SaveScore = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-    const SaveScore = () => {
-        axios.put(`${BASE_URL}/scores`, scoreUpdate)
-        .then((response) => {
-            console.log(`Filme atualizado com sucesso: ${response.data}`);
+        const inpEmail = (event.target as any).email.value;
+        const inpScore = (event.target as any).score.value;
+
+        axios.put(`${BASE_URL}/scores`, {
+            movieId: movieId,
+            email: inpEmail,
+            score: inpScore
         })
-        .catch((error) => {
-            console.log(`Não foi possivel obter obter dados do filme: ${error}`);
-        });
+            .catch((error) => {
+                console.log(`Não foi possivel obter obter dados do filme: ${error}`);
+            });
     }
 
     return (
@@ -44,24 +44,24 @@ const FormCard = ( { movieId} : Props ) => {
             <div className="card dsmovie-form-card mt-4 mb-5">
                 <img className="card-img-top rounded-top" src={movie?.image} alt="" />
                 <p className="dsmovie-card-text mt-3">{movie?.title}</p>
-                <form onSubmit={(event) => { event.preventDefault(); SaveScore() }} className='container mt-2'>
+                <form onSubmit={(event) => SaveScore(event)} className='container mt-2'>
                     <div className="form-group">
-                        <label className='dsmovie-email' htmlFor="dsmovie-form-email">Informe seu email</label>
-                        <input type="email" className="form-control dsmovie-input-email" id="dsmovie-form-email" aria-describedby="emailHelp" onChange={(event) =>{ scoreUpdate.email = event.target.value }}></input>
+                        <label className='dsmovie-email' htmlFor="email">Informe seu email</label>
+                        <input required type="email" className="form-control dsmovie-input-email" id="email" aria-describedby="emailHelp"></input>
                     </div>
                     <div className="form-group mt-3">
-                        <label className='dsmovie-text-score' htmlFor="dsmovie-form-score">Informe sua avaliação</label>
-                        <select className="form-control dsmovie-select-score" id='dsmovie-form-score' onChange={(event) => { scoreUpdate.score = event.target.value }}>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
+                        <label className='dsmovie-text-score' htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control dsmovie-select-score" id='score'>
+                            <option value={1}>1</option>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                            <option value={4}>4</option>
+                            <option value={5}>5</option>
                         </select>
                     </div>
                     <div className='dsmovie-buttons'>
                         <button type="submit" className='dsmovie-btn-salvar'>Salvar</button>
-                        <button type='submit' className='dsmovie-btn-cancelar'>Cancelar</button>
+                        <Link to='/' type='submit' className='dsmovie-btn-cancelar'>Cancelar</Link>
                     </div>
                 </form>
             </div>
